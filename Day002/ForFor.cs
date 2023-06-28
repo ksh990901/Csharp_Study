b4f434eebed3f34f98b9ff328f76963fa17d6f0f
ksh990901 <137870195+ksh990901@users.noreply.github.com>
2023-06-28 16:26:42 +0900

    Create ForFor.cs
    업로드합니다

---
+++ Day002/ForFor.cs
@@ -0,0 +1,26 @@
+namespace ForFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            for(int i = 0; i < 25;  i++) 
+            {
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+
+            cnt = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++) ;
+                {
+                    cnt++;
+                }
+               
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
