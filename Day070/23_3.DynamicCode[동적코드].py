89c18801b20e4f8fd7d2669e7da535f03762e183
ksh990901 <137870195+ksh990901@users.noreply.github.com>
2023-10-13 23:38:04 +0900

    Create 23_3.DynamicCode[동적코드].py

---
+++ Day070/23_3.DynamicCode[동적코드].py
@@ -0,0 +1,12 @@
+# # 동적 코드
+# # 프로그램 실행 중 여러가지 상황에서 유연하게 프로그래밍 할 수 있는 기능을 구현 할 수 있다.
+
+result = eval("1+2+6*8")
+print(result) # 51
+
+a = 2
+print(eval("a + 3")) # 5
+
+lists = print(eval("['A','B','C']"))
+print(lists)
+print(lists)
